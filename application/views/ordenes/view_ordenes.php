328d095cfdbc715fea18461b91030070f6e35bb5
Williams <w.contreras.f@gmail.com>
2016-04-22 20:30:48 -0300

    * pre borrado de ejemplo de checkbox con textbox

--- application/views/ordenes/view_ordenes.php
+++ application/views/ordenes/view_ordenes.php
@@ -26,11 +26,48 @@
       <div class="box-body">
         <div id="mensaje"></div>
         <!--<div class="box box-primary">-->
+        <table>
+          <tr>
+            <td colspan="3" style="text-align: center;">Leyenda Iconos</td>
+          </tr>
+          <tr>            
+            <td>Preview Orden <button type="button" title="Ver Orden" class="btn btn-info btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button>&nbsp;</td>
+            <td>Cerrar Orden <button type="button" title="Ver Orden" class="btn btn-warning btn-xs"><span class="glyphicon glyphicon-ok"></span></button>&nbsp;</td>
+            <td>Editar Orden <button type="button" title="Ver Orden" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-edit"></span></button>&nbsp;</td>
+          </tr>
+        </table>
+        <br>
+        <div style="height: 6em; width: 12em; overflow: auto;">
+          <div style="width: 12em; float: left;">
+            <input id="cheese" type="checkbox" name="ingredients[]" value="Cheese" />
+            <label for="cheese">Cheese</label>
+            <input type="text" value="" size="10px">
+          </div>
+          <br />
+          <div style="width: 12em; float: left;">
+            <input id="olives" type="checkbox" name="ingredients[]" value="Olives" />
+            <label for="olives">Olives</label>
+            <input type="text" value="" size="10px">
+          </div>
+          <br />          
+          <div style="width: 12em; float: left;">
+            <input id="pepperoni" type="checkbox" name="ingredients[]" value="Pepperoni" />
+            <label for="pepperoni">Pepperoni</label>
+            <input type="text" value="" size="10px">
+          </div>
+          <br />
+          <div style="width: 12em; float: left;">            
+            <input id="anchovies" type="checkbox" name="ingredients[]" value="Anchovies" />
+            <label for="anchovies">Anchovies</label>
+            <input type="text" value="" size="10px">
+          </div>
+        </div>
         <table id="ordenes" border="0" cellpadding="0" cellspacing="0" width="100%" class="pretty">
           <thead>
             <tr>
               <th>Folio</th>
               <th>Fecha Ingreso</th>
+              <th>Fecha Agenda</th>
               <th>Cliente</th>
               <!--<th>Fono</th>-->
               <th>Region-Comuna</th>
@@ -54,9 +91,27 @@
                     $adm_estado = 'Abierta';
                   }
 
+                  if($orden->fecha_agenda==null){
+                    $f_agenda = 'Sin Agenda';
+                  }else{
+                    $f_agenda = $orden->fecha_agenda;
+                  }
+
+                  if($f_agenda == date('Y-m-j')){
+                    $f_color = 'success';
+                  }elseif($f_agenda < date('Y-m-j')){
+                    $f_color = 'danger';
+                  }elseif($f_agenda > date('Y-m-j')){
+                    $f_color = 'info';
+                  }elseif($f_agenda == 'Sin Agenda'){
+                    $f_color = 'warning';
+                  }
+
+                  // style="background-color: '.$f_color.';"
                   echo '<tr>';
                   echo '<td>'.$orden->folio.'</td>';
                   echo '<td>'.$orden->fecha_ingreso.'</td>';
+                  echo '<td><button type="button" class="btn btn-block btn-'.$f_color.' btn-xs"><span class="fa fa-clock-o"></span> '.$f_agenda.'</button></td>';
                   echo '<td>'.$orden->cliente.'</td>';
                   //echo '<td>'.$orden->fono_cli.'</td>';
                   echo '<td>'.$orden->reg_comu.'</td>';
@@ -65,13 +120,13 @@
                   echo '<td>'.$orden->estado.'</td>';
                   echo '<td>'.$adm_estado.'</td>';
 
-                  if($this->session->userdata('TIPOUSUARIO') == 1){
+                  if($this->session->userdata('TIPOUSUARIO') == 1 || $this->session->userdata('TIPOUSUARIO') == 2){
                     $p_row = '<a href="orden/editarIngreso/'.$nrofolio.'"><button type="button" title="Editar Orden" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-edit"></span></button></a>';
                   }else{
                     $p_row = '<a href="editarIngreso/'.$nrofolio.'"><button type="button" title="Editar Orden" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-edit"></span></button></a>'; 
                   }
 
-                  if($this->session->userdata('TIPOUSUARIO')==1){
+                  if($this->session->userdata('TIPOUSUARIO')==1 || $this->session->userdata('TIPOUSUARIO')==2){
                     if($orden->estado_adm==1){
                       $p_adm = '<button type="button" title="Cerrar Orden" class="btn btn-warning btn-xs" id="btnPreview" disabled><span class="glyphicon glyphicon-ok"></span></button>&nbsp;';
                     }else{
@@ -89,8 +144,6 @@
                         '</td>';
                   echo '</tr>';
                 }
-              }else{
-                echo '<tr><td colspan=9><center>No Existen Ordenes Para Mostrar</center></td></tr>';
               }
             ?>
           </tbody>
@@ -102,7 +155,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Cerrar</span></button>
                         <h4 class="modal-title" id="myModalLabel">Visualizacion Datos Folio</h4>
                     </div>
                     <div class="modal-body">
@@ -144,15 +197,6 @@
                           <tr>
                               <td>
                                   <div class="form-group has-feedback">
-                                      <label for="in_sga">SGA*</label>
-                                      <!--<input type="text" id="txtNombreD" name="txtNombreD" placeholder="Ingrese segundo nombre" required="required" class="form-control" 
-                                          oninvalid="this.setCustomValidity('Campo segundo nombre obligatorio')" title="Debe ingresar segundo nombre de la persona">-->
-                                      <input type="text" id="txt_sga" class="form-control" readonly>
-                                  </div>
-                              </td>
-                              <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                              <td>
-                                  <div class="form-group has-feedback">
                                       <label for="in_ingreso">Fecha Ingreso*</label>
                                       <!--<input type="text" id="txtApPat" name="txtApPat" placeholder="Ingrese apellido paterno" required="required" class="form-control" 
                                           oninvalid="this.setCustomValidity('Campo Apellido Obligatorio')" title="Debe ingresar apellido paterno de la persona" style="width:300px;">-->
@@ -162,10 +206,19 @@
                               <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
                               <td>
                                   <div class="form-group has-feedback">
-                                      <label for="in_entrega">Fecha Entrega*</label>
+                                      <label for="in_entrega">Fecha Agenda*</label>
                                       <!--<input type="text" id="txtApMat" name="txtApMat" placeholder="Ingrese apellido materno" required="required" class="form-control" 
                                           oninvalid="this.setCustomValidity('Campo Apellido Obligatorio')" title="Debe ingresar materno paterno de la persona" style="width:300px;">-->
                                       <input type="text" id="txt_entrega" class="form-control" readonly>
+                                  </div>
+                              </td>
+                              <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                              <td>
+                                  <div class="form-group has-feedback">
+                                      <label for="in_bloque_agenda">Bloque Agenda*</label>
+                                      <!--<input type="text" id="txtApMat" name="txtApMat" placeholder="Ingrese apellido materno" required="required" class="form-control" 
+                                          oninvalid="this.setCustomValidity('Campo Apellido Obligatorio')" title="Debe ingresar materno paterno de la persona" style="width:300px;">-->
+                                      <input type="text" id="txt_bloque_agenda" class="form-control" readonly>
                                   </div>
                               </td>
                           </tr>
@@ -428,7 +481,7 @@
           <div class="modal-dialog">
               <div class="modal-content">
                   <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Cerrar</span></button>
                       <h4 class="modal-title" id="myModalLabel">Cerrar Orden</h4>
                   </div>
                   <div class="modal-body">
@@ -468,11 +521,19 @@
           "scrollX": true,
           "searching": false,
           "bSort": false,
+          "language": {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"//C:\wamp\www\siad_pyme\plugins\datatables\i18n
+          },
           "columnDefs": [
             { "width": "1%", "targets": 0 },
             { "width": "2%", "targets": 1 },
-            { "width": "3%", "targets": 7 },
-            { "width": "10%", "targets": 8 }
+            { "width": "2%", "targets": 2 },
+            { "width": "3%", "targets": 8 },
+            { "width": "10%", "targets": 9 },
+            {
+              "data": "",
+              "defaultContent": ""
+            }            
           ]
           /*"columns": [
                       { "width": "1%" },
