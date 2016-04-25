aab8cfe5946ce8421a664529b22060b3dd6315c8
Williams <w.contreras.f@gmail.com>
2016-04-25 11:58:50 -0300

    * modificacion de constante para que solo muestre logo de claro

--- application/views/constant.php
+++ application/views/constant.php
@@ -2,7 +2,7 @@
 	const NOMBRE_EMPRESA            = 'Despacho Nacional';
 	const TITULO_PAGINA 			= 'SIAD PYME';
 	const EXTENSION_IMAGEN_FAVICON  = 'png';//png , gif, jpeg, jpg
-	const NOMBRE_IMAGEN_FAVICON     = 'Logophp.png';
+	const NOMBRE_IMAGEN_FAVICON     = 'Claro.png';
 	const LEYENDA_EXTRA             = '';
 	const CALLE                     = '';
 	const CP                        = '';
