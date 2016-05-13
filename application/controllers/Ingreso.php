56b0d84a64aa54a3670c4e0f4aec959dfa1e3739
Williams <w.contreras.f@gmail.com>
2016-05-13 12:19:34 -0300

    * se agrega para el tema decos ademas se corrigen inconsistencias al actualizar por campos que ya no se utilizaban, se agrega validacion fecha agenda al actualizar datos

--- application/controllers/Ingreso.php
+++ application/controllers/Ingreso.php
@@ -6,6 +6,7 @@ class Ingreso extends CI_Controller {
 		$this->load->model('Seguridad_model');
 		$this->load->model('Ingreso_model');
 		$this->load->model('Regcomu_model');
+		$this->load->model('Orden_model');
 		$this->load->helper('date');
 	}
 
@@ -14,9 +15,10 @@ class Ingreso extends CI_Controller {
       	$this->Seguridad_model->SessionActivo($url);
 		$this->load->view('constant');
 		$this->load->view('view_header');
-		$data['titulo']    = "Nuevo Ingreso";
+		$data['titulo'] = "Nuevo Ingreso";
 		$data['n_folio'] = base64_decode($nfolio);
 		$data['data_flag'] = "0";//para saber si es para update
+		$data['data_folio_agenda'] = '';
 		$this->load->view('ingreso/view_ingreso',$data);
 		$this->load->view('view_footer');
 	}
@@ -56,9 +58,23 @@ class Ingreso extends CI_Controller {
 		echo json_encode($plan);
 	}
 
+	public function centrales(){
+		$central = $this->Regcomu_model->GetListaCentral();
+		echo json_encode($central);
+	}
+
+	public function reparaciones(){
+		$repas = $this->Regcomu_model->GetListaReparacionTipo();
+		echo json_encode($repas);
+	}
+
+	public function codcierres(){
+		$cierres = $this->Regcomu_model->GetListaCodigCierre();
+		echo json_encode($cierres);
+	}
+
 	public function GuardaRegistro(){
-		$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-        
+		$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];        
         $this->Seguridad_model->SessionActivo($url);
 
 		$InsRegistro = json_decode($this->input->post('InsRegistro'));
@@ -78,54 +94,102 @@ class Ingreso extends CI_Controller {
 
 		if($InsRegistro->es_update==1){
 			//realizo update
-			$ActualizaRegistro = array(
-				//'in_sga'              => $InsRegistro->p_sga,
-				'in_ingreso'          => $InsRegistro->p_ingreso,
-				'in_entrega'          => $InsRegistro->p_entrega,
-				'in_cliente'          => $InsRegistro->p_cliente,
-				'in_rut'              => $RutSinGuion,//InsRegistro->p_rut,
-				'in_direccion'        => $InsRegistro->p_direccion,
-				'in_region'           => $RegionComuna[0],//$InsRegistro->p_region,
-				'in_comuna'           => $InsRegistro->p_comuna,//$RegionComuna[1],//$InsRegistro->p_comuna,
-				'in_nombre'           => $InsRegistro->p_nombre,
-				'in_fono'             => $InsRegistro->p_fono,
-				'in_plan_net'         => $InsRegistro->p_plan_net,
-				'in_plan_net_adic'    => $InsRegistro->p_plan_net_adic,
-				'in_plan_fono'        => $InsRegistro->p_plan_fono,
-				'in_plan_fono_adic'   => $InsRegistro->p_plan_fono_adic,
-				'in_plan_fono_adict'  => $InsRegistro->p_plan_fono_adict,
-				'in_plan_tv'          => $InsRegistro->p_plan_tv,
-				'in_deco_basico'      => $InsRegistro->p_deco_basico,
-				'in_plan_tv_adic'     => $InsRegistro->p_plan_tv_adic,
-				'in_plan_tv_adict'    => $InsRegistro->p_plan_tv_adict,
-				'in_deco_hd_basico'   => $InsRegistro->p_deco_hd_basico,
-				'in_deco_hd_full'     => $InsRegistro->p_deco_hd_full,
-				'in_plan_tv_pack'     => $InsRegistro->p_plan_tv_pack,
-				'in_central_tf'       => $InsRegistro->p_central_tf,
-				'in_lineas_asignadas' => $InsRegistro->p_lineas_asignadas,
-				'in_fecha_operacion'  => $InsRegistro->p_fecha_operacion,
-				'in_vende'            => $InsRegistro->p_vende
-			);
-			
-			$ActualizaRegistroDetalle = array(
-				'in_proyecto'            => $InsRegistro->p_proyecto,
-				'indet_fecha_registro'   => date('Y-m-j H:i:s'),
-				'indet_usr_registro'     => $this->session->userdata('ID'),
-				'indet_usr_empresa'      => $this->session->userdata('ALIADORUT'),
-				'indet_observacion'      => $InsRegistro->p_observacion,
-				'in_estado'           	 => $InsRegistro->p_estado
-			);
+			//compruebo que campo observacion no este en blanco
+			if($InsRegistro->p_observacion == null || $InsRegistro->p_observacion == ""){
+				$response["campo"] = "indet_observacion";
+				$response["error_msg"] = "<div class='alert alert-danger text-center' alert-dismissable> <button type='button' class='close' data-dismiss='alert'>&times;</button><span class='glyphicon glyphicon-remove'></span> &nbsp; El campo observacion no puede estar en blanco</div>";
+				echo json_encode($response);
+			}elseif($InsRegistro->p_estado == null || $InsRegistro->p_estado == "" || $InsRegistro->p_estado == "0"){
+				$response["campo"] = "in_estado";
+				$response["error_msg"] = "<div class='alert alert-danger text-center' alert-dismissable> <button type='button' class='close' data-dismiss='alert'>&times;</button><span class='glyphicon glyphicon-remove'></span> &nbsp; Debe seleccionar un estado de la orden</div>";
+				echo json_encode($response);
+			}else{
+				$ActualizaRegistro = array(
+					'in_entrega'          => $InsRegistro->p_entrega,
+					'in_cliente'          => $InsRegistro->p_cliente,
+					'in_rut'              => $RutSinGuion,
+					'in_direccion'        => $InsRegistro->p_direccion,
+					'in_nombre'           => $InsRegistro->p_nombre,
+					'in_fono'             => $InsRegistro->p_fono,
+					'in_plan_net_adic'    => $InsRegistro->p_plan_net_adic,
+					'in_plan_fono_adicu'  => $InsRegistro->p_plan_fono_adicu,
+					'in_plan_fono_adicd'  => $InsRegistro->p_plan_fono_adicd,
+					//'in_plan_tv_adicu'    => $InsRegistro->p_plan_tv_adicu,
+					//'in_plan_tv_adicd'    => $InsRegistro->p_plan_tv_adicd,
+					'in_plan_pack'		  => $InsRegistro->p_plan_tv_pack,
+					//'in_central_tf'       => $InsRegistro->p_central_tf,
+					//'in_lineas_asignadas' => $InsRegistro->p_lineas_asignadas,
+					//'in_fecha_cierre'     => $InsRegistro->p_fecha_cierre,
+					'in_vende'            => $InsRegistro->p_vende,
+					'in_hora_ingreso'     => date('H:i:s'),						
+					'tt_id'               => $InsRegistro->tt_id,						
+					'id_comuna'           => $InsRegistro->p_comuna,
+					'id_region'           => $RegionComuna[0],
+					'plan_id'             => $InsRegistro->plan_id,
+					'deco_id'             => $InsRegistro->deco_id
+				);
+				
+				$ActualizaRegistroDetalle = array(
+					'in_proyecto'            => $InsRegistro->p_proyecto,
+					'indet_fecha_registro'   => date('Y-m-j H:i:s'),
+					'indet_usr_registro'     => $this->session->userdata('ID'),
+					'indet_usr_empresa'      => $this->session->userdata('ALIADORUT'),
+					'indet_observacion'      => $InsRegistro->p_observacion,
+					'in_estado'           	 => $InsRegistro->p_estado
+				);
 
-			$this->Ingreso_model->UpdateIngreso($ActualizaRegistro, $InsRegistro->p_proyecto);
-			$this->Ingreso_model->SaveIngresoDetalle($ActualizaRegistroDetalle);
-			
-			$response["error_msg"]   = "<div class='alert alert-success text-center' alert-dismissable> <button type='button' class='close' data-dismiss='alert'>&times;</button><span class='glyphicon glyphicon-ok'></span> &nbsp; Informacion Actualizada Correctamente</div>";// form-control-feedback
-			echo json_encode($response);
+				$ActualizaRegistroDecoAdic = array(				
+					'decoa_sd'	           => $InsRegistro->deco_sd,
+					'decoa_hd'             => $InsRegistro->deco_hd,
+					'decoa_tvr'            => $InsRegistro->deco_tvr,
+					'decoa_stnd'           => $InsRegistro->deco_std,
+					'decoa_usr_registro'   => $this->session->userdata('ID'),
+					'decoa_usr_empresa'    => $this->session->userdata('ALIADORUT'),
+					'decoa_fecha_registro' => date('Y-m-j H:i:s')
+				);
+
+				if($InsRegistro->p_entrega != null){
+					$agendaFolio = $this->Orden_model->GetAgendaByFolio($InsRegistro->p_proyecto);
+					foreach ($agendaFolio as $value) {
+						# valido que al actualizar no ingrese un nuevo registro de agenda en caso de que bloque y fecha sean iguales
+						if(($value->reagenda_fecha != $InsRegistro->p_entrega) && ($value->reagenda_bloque != $InsRegistro->p_bloque)){
+							$GuardaRegistroAgenda = array(
+								'in_proyecto'           => $InsRegistro->p_proyecto,
+								'reagenda_fecha'        => $InsRegistro->p_entrega,
+								'reagenda_bloque'       => $InsRegistro->p_bloque,
+								'reagenda_modificacion' => date('Y-m-j H:i:s'),
+								'reagenda_usr_registro' => $this->session->userdata('ID'),
+								'reagenda_usr_empresa'  => $this->session->userdata('ALIADORUT')
+							);
+
+							$this->Ingreso_model->SaveIngresoAgenda($GuardaRegistroAgenda);
+							break;
+						}else{
+							break;
+						}
+					}										
+				}
+
+				$this->Ingreso_model->UpdateIngreso($ActualizaRegistro, $InsRegistro->p_proyecto);
+				$this->Ingreso_model->SaveIngresoDetalle($ActualizaRegistroDetalle);
+				$this->Ingreso_model->UpdateDetalleDeco($ActualizaRegistroDecoAdic, $InsRegistro->p_proyecto);
+				
+				$response["error_msg"]   = "<div class='alert alert-success text-center' alert-dismissable> <button type='button' class='close' data-dismiss='alert'>&times;</button><span class='glyphicon glyphicon-ok'></span> &nbsp; Informacion Actualizada Correctamente</div>";// form-control-feedback
+				echo json_encode($response);
+			}			
 		}else{
 			//compruebo que campo observacion no este en blanco
 			if($InsRegistro->p_observacion == null || $InsRegistro->p_observacion == ""){
 				$response["campo"] = "indet_observacion";
 				$response["error_msg"] = "<div class='alert alert-danger text-center' alert-dismissable> <button type='button' class='close' data-dismiss='alert'>&times;</button><span class='glyphicon glyphicon-remove'></span> &nbsp; El campo observacion no puede estar en blanco</div>";
+				echo json_encode($response);
+			}elseif($InsRegistro->p_estado == null || $InsRegistro->p_estado == "" || $InsRegistro->p_estado == "0"){
+				$response["campo"] = "in_estado";
+				$response["error_msg"] = "<div class='alert alert-danger text-center' alert-dismissable> <button type='button' class='close' data-dismiss='alert'>&times;</button><span class='glyphicon glyphicon-remove'></span> &nbsp; Debe seleccionar un estado de la orden</div>";
+				echo json_encode($response);
+			}elseif($InsRegistro->p_proyecto == null || $InsRegistro->p_proyecto == "" || $InsRegistro->p_proyecto == "0"){
+				$response["campo"] = "in_proyecto";
+				$response["error_msg"] = "<div class='alert alert-danger text-center' alert-dismissable> <button type='button' class='close' data-dismiss='alert'>&times;</button><span class='glyphicon glyphicon-remove'></span> &nbsp; Debe ingresar un numero de Proyecto/Solot</div>";
 				echo json_encode($response);
 			}else{
 				//compruebo que no exista e inserto
@@ -135,61 +199,34 @@ class Ingreso extends CI_Controller {
 					echo json_encode($response);
 				}else{
 					$GuardaRegistro  = array(
-						/*'in_proyecto' 	      => $InsRegistro->p_proyecto,
-						//'in_sga'              => $InsRegistro->p_sga,
+						'in_proyecto'         => $InsRegistro->p_proyecto,
 						'in_ingreso'          => $InsRegistro->p_ingreso,
 						//'in_entrega'          => $InsRegistro->p_entrega,
 						'in_cliente'          => $InsRegistro->p_cliente,
-						'in_rut'              => $RutSinGuion,//InsRegistro->p_rut,
-						'in_direccion'        => $InsRegistro->p_direccion,
-						'in_region'           => $RegionComuna[0],//$InsRegistro->p_region,
-						'in_comuna'           => $InsRegistro->p_comuna,//$RegionComuna[1],//$InsRegistro->p_comuna,
-						'in_nombre'           => $InsRegistro->p_nombre,
-						'in_fono'             => $InsRegistro->p_fono,
-						'in_plan_net'         => $InsRegistro->p_plan_net,
-						'in_plan_net_adic'    => $InsRegistro->p_plan_net_adic,
-						'in_plan_fono'        => $InsRegistro->p_plan_fono,
-						'in_plan_fono_adic'   => $InsRegistro->p_plan_net_adic,
-						'in_plan_fono_adict'  => $InsRegistro->p_plan_fono_adict,
-						'in_plan_tv'          => $InsRegistro->p_plan_tv,
-						'in_deco_basico'      => $InsRegistro->p_deco_basico,
-						'in_plan_tv_adic'     => $InsRegistro->p_plan_tv_adic,
-						'in_plan_tv_adict'    => $InsRegistro->p_plan_tv_adict,
-						'in_deco_hd_basico'   => $InsRegistro->p_deco_hd_basico,
-						'in_deco_hd_full'     => $InsRegistro->p_deco_hd_full,
-						'in_plan_tv_pack'     => $InsRegistro->p_plan_tv_pack,
-						'in_central_tf'       => $InsRegistro->p_central_tf,
-						'in_lineas_asignadas' => $InsRegistro->p_lineas_asignadas,
-						'in_fecha_operacion'  => $InsRegistro->p_fecha_operacion,
-						'in_vende'            => $InsRegistro->p_vende,
-						//'in_estado'           => $InsRegistro->p_estado,
-						'in_tipo_trabajo'	  => $InsRegistro->p_ttrabajo*/
-
-						'in_proyecto'         => $InsRegistro->p_proyecto,
-						'in_ingreso'          => $InsRegistro->p_ingreso,
-						'in_entrega'          => $InsRegistro->p_entrega,
-						'in_cliente'          => $InsRegistro->p_cliente,
 						'in_rut'              => $RutSinGuion,
-						'in_direccion'        => $InsRegistro->p_direccion,
 						'in_nombre'           => $InsRegistro->p_nombre,
 						'in_fono'             => $InsRegistro->p_fono,
+						'in_direccion'        => $InsRegistro->p_direccion,
+						'in_direccion_t'      => $InsRegistro->p_direccion_t,
 						'in_plan_net_adic'    => $InsRegistro->p_plan_net_adic,
-						'in_plan_fono_adicu'  => $InsRegistro->p_plan_net_adicu,
-						'in_plan_fono_adicd'  => $InsRegistro->in_plan_fono_adicd,
-						'in_plan_tv_adicu'    => $InsRegistro->in_plan_tv_adicu,
-						'in_plan_tv_adicd'    => $InsRegistro->in_plan_tv_adicd,
+						'in_plan_fono_linea'  => $InsRegistro->p_plan_fono_adicu,
+						'in_plan_fono_ext'    => $InsRegistro->p_plan_fono_adicd,
+						//'in_plan_tv_adicu'    => $InsRegistro->p_plan_tv_adicu,
+						//'in_plan_tv_adicd'    => $InsRegistro->p_plan_tv_adicd,
 						'in_plan_pack'		  => $InsRegistro->p_plan_tv_pack,
-						'in_central_tf'       => $InsRegistro->p_central_tf,
-						'in_lineas_asignadas' => $InsRegistro->p_lineas_asignadas,
-						'in_fecha_cierre'     => $InsRegistro->p_fecha_cierre,
+						//'in_central_tf'       => $InsRegistro->p_central_tf,
+						//'in_lineas_asignadas' => $InsRegistro->p_lineas_asignadas,
+						//'in_fecha_cierre'     => $InsRegistro->p_fecha_cierre,
 						'in_vende'            => $InsRegistro->p_vende,
-						'in_hora_ingreso'     => date('H:i:s'),						
+						'in_hora_ingreso'     => date('H:i:s'),
+						'plan_id'             => $InsRegistro->plan_id,
 						'tt_id'               => $InsRegistro->tt_id,						
 						'id_comuna'           => $InsRegistro->p_comuna,
 						'id_region'           => $RegionComuna[0],
-						'plan_id'             => $InsRegistro->plan_id,
-						'deco_id'             => $InsRegistro->deco_id       
-
+						'rt_id'               => $InsRegistro->p_rep_tipo,
+						'vt_codigo'           => $InsRegistro->p_rep_codi
+						//'plan_id'             => $InsRegistro->plan_id,
+						//'deco_id'             => $InsRegistro->deco_id
 					);
 
 					$GuardaRegistroDetalle = array(
@@ -202,11 +239,12 @@ class Ingreso extends CI_Controller {
 					);
 
 					$GuardaRegistroDecoAdic = array(
-						'in_proyecto'          => $InsRegistro->p_proyecto,
+						'in_proyecto'          => $InsRegistro->p_proyecto,						
+						'deco_id'              => $InsRegistro->deco_id,
 						'decoa_sd'	           => $InsRegistro->deco_sd,
 						'decoa_hd'             => $InsRegistro->deco_hd,
 						'decoa_tvr'            => $InsRegistro->deco_tvr,
-						'decoa_stnd'           => $InsRegistro->deco_std.
+						'decoa_stnd'           => $InsRegistro->deco_std,
 						'decoa_usr_registro'   => $this->session->userdata('ID'),
 						'decoa_usr_empresa'    => $this->session->userdata('ALIADORUT'),
 						'decoa_fecha_registro' => date('Y-m-j H:i:s')
@@ -223,7 +261,20 @@ class Ingreso extends CI_Controller {
 						);
 
 						$this->Ingreso_model->SaveIngresoAgenda($GuardaRegistroAgenda);
-					}				
+					}
+
+					if($InsRegistro->p_central_tf != "0" || $InsRegistro->p_central_tf != null){
+						$GuardaRegistroCentral = array(
+							'in_proyecto'      =>$InsRegistro->p_proyecto,
+							'ctf_id'           =>$InsRegistro->p_central_tf,
+							'dqty_linea'       =>$InsRegistro->p_lineas_asignadas,
+							'dqty_anexo'       =>$InsRegistro->p_lineas_anexos,
+							'ctf_usr_registro' =>$this->session->userdata('ID'),
+							'ctf_usr_empresa'  =>$this->session->userdata('ALIADORUT')
+						);
+
+						$this->Ingreso_model->SaveIngresoCentral($GuardaRegistroCentral);
+					}			
 
 					$this->Ingreso_model->SaveIngreso($GuardaRegistro);
 					$this->Ingreso_model->SaveIngresoDetalle($GuardaRegistroDetalle);				
