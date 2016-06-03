57f087f7ada044ff5430d5527677c64656df04db
Williams <w.contreras.f@gmail.com>
2016-06-03 09:55:20 -0400

    * cambio de consulta de obtencion de ordenes para poder generar excel sin problemas

--- application/models/Orden_model.php
+++ application/models/Orden_model.php
@@ -9,17 +9,37 @@ class Orden_model extends CI_Model {
 
 	//lista ordenes para admin
 	public function ListarOrdenesAdmin(){
-		$sql = "select tbl_sp_ingreso.*, in_estado, (select reagenda_fecha from tbl_sp_reagenda where reagenda_id = (select max(reagenda_id) from tbl_sp_reagenda where in_proyecto = tbl_sp_ingreso.in_proyecto)) as fecha_agenda from tbl_sp_ingreso left join tbl_sp_detalle 
-				on (tbl_sp_ingreso.in_proyecto = tbl_sp_detalle.in_proyecto) 
-				where tbl_sp_detalle.indet_fecha_registro in (select max(indet_fecha_registro) from tbl_sp_detalle where tbl_sp_ingreso.in_proyecto = tbl_sp_detalle.in_proyecto) 
-				order by (select reagenda_fecha from tbl_sp_reagenda where reagenda_id = (select max(reagenda_id) from tbl_sp_reagenda where in_proyecto = tbl_sp_ingreso.in_proyecto))";
+		$sql = "select tbl_sp_ingreso.*,
+						in_estado,
+						(select reagenda_fecha from tbl_sp_reagenda where reagenda_id = (select max(reagenda_id) from tbl_sp_reagenda where in_proyecto = tbl_sp_ingreso.in_proyecto)) as fecha_agenda,
+						plan_alta,
+						tt_nombre,
+						SIAD.regiones.descripcion as region,
+						SIAD.comunas.descripcion as comuna,
+						rt_descripcion,
+						vt_descripcion,
+						tfa_descripcion,
+						tcv_nombre,
+						est_descripcion
+						from tbl_sp_ingreso left join tbl_sp_detalle on (tbl_sp_ingreso.in_proyecto = tbl_sp_detalle.in_proyecto)
+						left join tbl_sp_planes on (tbl_sp_ingreso.plan_id = tbl_sp_planes.plan_id)
+						left join tbl_sp_tipo_trabajo on (tbl_sp_ingreso.tt_id = tbl_sp_tipo_trabajo.tt_id)
+						left join SIAD.regiones on (SIAD.regiones.id = tbl_sp_ingreso.id_region)
+						left join SIAD.comunas on (SIAD.comunas.id = tbl_sp_ingreso.id_comuna)
+						left join tbl_sp_reparacion_tipo on (tbl_sp_ingreso.rt_id = tbl_sp_reparacion_tipo.rt_id)
+						left join tbl_sp_cierre_vt on (tbl_sp_cierre_vt.vt_id = tbl_sp_ingreso.vt_codigo)
+						left join tbl_sp_tipo_falla on (tbl_sp_tipo_falla.tfa_id = tbl_sp_ingreso.tfa_id)
+						left join tbl_sp_tipo_canal_venta on (tbl_sp_tipo_canal_venta.tcv_id = tbl_sp_ingreso.tcv_id)
+						left join tbl_sp_estados on (tbl_sp_estados.est_id = in_estado)
+						where tbl_sp_detalle.indet_fecha_registro in (select max(indet_fecha_registro) from tbl_sp_detalle where tbl_sp_ingreso.in_proyecto = tbl_sp_detalle.in_proyecto)
+						order by (select reagenda_fecha from tbl_sp_reagenda where reagenda_id = (select max(reagenda_id) from tbl_sp_reagenda where in_proyecto = tbl_sp_ingreso.in_proyecto))";
 	    $query = $this->db->query($sql);
 
 		return $query->result();
 	}
 
 	public function GetOrdenByFolio($folio){
-		/*$this->db->where('in_proyecto',$folio);		
+		/*$this->db->where('in_proyecto',$folio);
 	 	return $this->db->get('tbl_sp_ingreso')->row();*/
 
 	 	$this->db->select('tbl_sp_ingreso.*, tbl_sp_reagenda.reagenda_fecha, tbl_sp_reagenda.reagenda_bloque');
@@ -39,6 +59,26 @@ class Orden_model extends CI_Model {
 	 	return $this->db->get('tbl_sp_detalle')->result();
 	}
 
+	public function GetOrdenByFolioDeco($folio){
+		$this->db->where('in_proyecto',$folio);
+	 	return $this->db->get('tbl_sp_deco_adicional')->row();
+	}
+
+	public function GetAgendaByFolio($folio){
+		/*$this->db->where('in_proyecto',$folio);
+		$this->db->order_by('reagenda_id','desc');
+	 	return $this->db->get('tbl_sp_reagenda')->result();*/
+
+	 	$this->db->select('reagenda_fecha, bloque_descripcion, reagenda_modificacion, reagenda_bloque, concat(nombres," ",paterno) as reagenda_usr_registro');
+	 	$this->db->from('tbl_sp_reagenda');
+	 	$this->db->join('SIAD.usuarios','SIAD.usuarios.id = reagenda_usr_registro','left');
+	 	$this->db->join('tbl_sp_bloques','bloque_id = reagenda_bloque','left');
+	 	$this->db->where('in_proyecto',$folio);
+	 	$this->db->order_by('reagenda_id','desc');
+	 	$query = $this->db->get();
+
+      	return $query->result();
+	}
 	/*public function GetOrdenByFolioDetFirst($folio){
 		$this->db->where('in_proyecto',$folio);
 		$this->db->order_by('indet_fecha_registro','desc');
@@ -49,8 +89,12 @@ class Orden_model extends CI_Model {
 	//obtiene nombre aliado pyme de acuerdo a la comuna
 	/*public function GetNombreAliadoByComuna($comu){
 		$this->db_inf->where('comuna',$comu);
-	 	return $this->db_inf->get('encargados_zona')->row();	
+	 	return $this->db_inf->get('encargados_zona')->row();
 	}*/
+	public function GetCentralByFolio($folio){
+		$this->db->where('in_proyecto',$folio);
+	 	return $this->db->get('tbl_sp_detalle_central')->row();
+	}
 
 	public function GetNombreAliadoByComuna($comu){
 		$this->db_inf->select('aliado_nombre');
@@ -63,10 +107,10 @@ class Orden_model extends CI_Model {
 
     public function GetNombreAliadoById($idaliado){
     	$this->db->select('inf_despacho_nacional.tbl_aliado.aliado_nombre');
-      	$this->db->from('inf_despacho_nacional.tbl_aliado');      	
+      	$this->db->from('inf_despacho_nacional.tbl_aliado');
       	$this->db->where('inf_despacho_nacional.tbl_aliado.aliado_id in (select siad_pyme.tbl_sp_empresa_aliado.aliado_id from siad_pyme.tbl_sp_empresa_aliado where siad_pyme.tbl_sp_empresa_aliado.empresa_id = '.$idaliado.')');
       	$query = $this->db->get();
-      	return $query->row();	
+      	return $query->row();
     }
 
 	//obtiene comuna de acuerdo al id
@@ -100,6 +144,11 @@ class Orden_model extends CI_Model {
 	public function GetBloqueByIdBloque($bloque){
 		$this->db->where('bloque_id',$bloque);
 		return $this->db->get('tbl_sp_bloques')->row();
+	}
+
+	public function GetPlanByIdPlan($plan){
+		$this->db->where('plan_id',$plan);
+		return $this->db->get('tbl_sp_planes')->row();
 	}
 
 	public function UpdateEstadoAdmOrden($folio,$data){
